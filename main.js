6876a12d7661860649150bdfca962090e2f7e5d9
vini <barbosavinicius410@gmail.com>
2022-09-12 02:17:11 -0300

    update

---
+++ main.js
@@ -0,0 +1,22 @@
+//pegar todas as teclas
+const keys = document.querySelectorAll(".key")
+ console.log(keys);
+ 
+ 
+// tocar musica 
+
+function playnotas(event) {
+    //pegar a referencia da letra através do keycode
+    
+    //verificar se a tecla existe
+    
+    //play audio
+    
+}
+
+
+// clickar com mouse
+
+
+//clicar com teclado
+window.addEventListener("keydown", playnotas)
