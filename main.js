ec8359f2726d5d9ea13c4e1119e984759fc84cb2
vini <barbosavinicius410@gmail.com>
2022-09-12 15:40:43 -0300

    final update

--- main.js
+++ main.js
@@ -1,22 +1,60 @@
-//pegar todas as teclas
-const keys = document.querySelectorAll(".key")
- console.log(keys);
- 
- 
-// tocar musica 
+// pwgar todas as teclas
+const keys = document.querySelectorAll(".key");
 
-function playnotas(event) {
-    //pegar a referencia da letra através do keycode
-    
-    //verificar se a tecla existe
-    
-    //play audio
-    
+function playNote(event) {
+
+    let audioKeyCode = getKeyCode(event);
+
+    // ver se a key foi pressionada ou clicada
+    const key = document.querySelector(`.key[data-key="${audioKeyCode}"]`);
+
+    // verificar se existe
+    const cantFoundAnyKey = !key;
+
+    if (cantFoundAnyKey) {
+        return;
+    }
+
+    addPlayingClass(key);
+    playAudio(audioKeyCode);
 }
 
+function addPlayingClass(key) {
+    key.classList.add('playing');
+}
 
-// clickar com mouse
+function getKeyCode(event) {
+    let keyCode;
 
+    const isKeyboard = event.type === "keydown";
+    if (isKeyboard) {
+        keyCode = event.keyCode;
+    } else {
+        keyCode = event.target.dataset.key;
+    }
 
-//clicar com teclado
-window.addEventListener("keydown", playnotas)
+    return keyCode;
+}
+
+function playAudio(audioKeyCode) {
+    const audio = document.querySelector(`audio[data-key="${audioKeyCode}"]`);
+    audio.currentTime = 0;
+    audio.play();
+}
+
+function removePlayingClass(event) {
+    event.target.classList.remove("playing");
+}
+
+function registerEvents() {
+    // click com o mouse
+    keys.forEach(function(key) {
+        key.addEventListener("click", playNote);
+        key.addEventListener("transitionend", removePlayingClass);
+    });
+
+    // teclado pressionado
+    window.addEventListener("keydown", playNote);
+}
+
+window.addEventListener("load", registerEvents);
